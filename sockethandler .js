0d9a8d836e580190c77e86e5a7d87f2ad6b571de
RAO-7 <raoabdullah221144@gmail.com>
2026-05-09 13:00:58 +0500

    Update sockethandler .js

--- sockethandler .js
+++ sockethandler .js
@@ -1,1 +1,14 @@
+const io = require('socket.io')(server);
+const simulator = new FleetSimulator(initialData);
 
+setInterval(() => {
+  const updatedState = simulator.tick();
+  io.emit('fleetUpdate', updatedState); // Broadcast to all roles [cite: 62]
+}, 1000); // 1Hz [cite: 10]
+
+io.on('connection', (socket) => {
+  socket.on('issueDirective', (data) => {
+    // Command role only logic [cite: 66, 68]
+    io.emit('captainDirective', data);
+  });
+});
